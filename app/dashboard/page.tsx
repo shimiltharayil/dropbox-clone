a705aad605cccf053a75286dbcf0738019ce61e9
Shimil Tharayil <109408967+shimiltharayil@users.noreply.github.com>
2024-01-14 18:36:48 +0530

    adding skeleton loading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import Dropzone from "@/components/Dropzone";
+import { db } from "@/firebase";
 import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 
 type Props = {};
 
 function Dashboard({}: Props) {
   const { userId } = auth();
+  const docResults = await getDocs(collection(db, "users", userId!, "files"));
   return (
     <div>
       <Dropzone />
